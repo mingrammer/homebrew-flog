766fdf05fede015453aa6cd098ca1195cc13801b
mingrammer <mingrammer@gmail.com>
2018-12-21 04:57:34 +0900

    Brew formula update for flog version v0.2.4

--- flog.rb
+++ flog.rb
@@ -1,9 +1,9 @@
 class Flog < Formula
   desc "A fake log generator for common log formats"
   homepage "https://github.com/mingrammer/flog"
-  url "https://github.com/mingrammer/flog/releases/download/v0.2.3/flog_0.2.3_darwin_amd64.tar.gz"
-  version "0.2.3"
-  sha256 "8aed360e6310c4006e4616e0d9dcfda5caf8981018166538a14bf2ae0bd7792f"
+  url "https://github.com/mingrammer/flog/releases/download/v0.2.4/flog_0.2.4_darwin_amd64.tar.gz"
+  version "0.2.4"
+  sha256 "49de7a50c4ca1f1c6f278e0bf1aeb8224e643d4308795e7834f0b5a18c6d1409"
 
   def install
     bin.install "flog"
